799d2ea2b6d8a9cf576ba106da77e54154d887ac
21f3002380 <21f3002380.ds.study.iitm.ac.in>
2026-02-09 00:27:23 +0530

    First files 09 Feb 2026

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-#hello
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('./index.html')
+
+app.run(debug=True)
